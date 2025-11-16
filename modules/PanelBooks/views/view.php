b46b7eb20e1226422fea7be520c31e2c00952151
Bhaswanth <bhaswanth22@gmail.com>
2025-11-16 16:02:53 +0530

    Added page listing in  book view

--- modules/PanelBooks/views/view.php
+++ modules/PanelBooks/views/view.php
@@ -21,7 +21,69 @@
     <div class="section-body">
         <?php $this->includes("includes/messages", null, "Auth") ?>
 
-        <p>Total Books are : <?= $pages->count(); ?></p>
+        <?php if(isset($pages) && $pages->count() > 0) : ?>
+            <?php $template->component("components/table", null, "Main") ?>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Type</th>
+                        <th>Order</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($pages->getData() as $page) : ?>
+                        <tr>
+                            <td><?= $page["id"] ?></td>
+                            <td><?= $page["title"] ?></td>
+                            <td>
+                                <span class="table-label <?= $page["type"] ?>"><?= ucfirst($page["type"]) ?></span>
+                            </td>
+                            <td>
+                                <form action="" method="POST" onsubmit="return confirm('Are you sure you want to update?')">
+                                    <?= form_method("PATCH") ?>
+                                    <input type="hidden" name="book_id" value="<?= $book["id"] ?>" />
+                                    <input type="hidden" name="page_id" value="<?= $page["id"] ?>" />
+                                    <select class="table-input" name="order_no">
+                                        <option value="" disabled <?= is_null($page["order_no"]) ? "selected" : "" ?>></option>
+                                        <?php for($i=1; $i<=$pages->count(); $i++) : ?>
+                                            <option 
+                                                value="<?= $i ?>" 
+                                                <?= $page["order_no"] == $i ? "selected" : "" ?>>
+                                                <?= $i ?>
+                                            </option>
+                                        <?php endfor ?>
+                                    </select>
+                                </form>
+                            </td>
+                            <td>
+                                <form action='' method="POST" onsubmit="return confirm('Are you sure you want to update?')">
+                                    <?= form_method("PATCH") ?>
+                                    <input type="hidden" name="status" value="<?= $page['status'] ?? 'draft' ?>" />
+                                    <?php if ($page["status"] === "published") : ?>
+                                        <button type="submit" class="btn btn-sm btn-success">Published</button>
+                                    <?php elseif ($page["status"] === "draft") : ?>
+                                        <button type="submit" class="btn btn-sm btn-warning">Draft</button>
+                                    <?php endif ?>
+                                </form>
+                            </td>
+                            <td>
+                                <a href="<?= route("panel.books.edit", ["id" => $book['id']]) ?>">
+                                    <button class="btn btn-sm btn-primary">Edit</button>
+                                </a>
+                                <?php if($page["type"] === "chapter") : ?>
+                                    <a href="">
+                                        <button class="btn btn-sm btn-info">View</button>
+                                    </a>
+                                <?php endif ?>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            <?php $template->endComponent() ?>
+        <?php endif ?>
     </div>
 </div>
 
